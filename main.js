b56fe52730ae491934d0930fd93d47637528e0bf
hamilton moore <hamlmoor@gmail.com>
2017-08-04 10:33:01 -0500

    completeed equals function

--- main.js
+++ main.js
@@ -6,16 +6,12 @@ var equals = document.querySelector(".equals");
 for (i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (e) {
         display.innerHTML += e.target.innerHTML;
-
     });
 }
-
 clear.addEventListener("click", function (e) {
     display.innerHTML = "";
-
 });
-
-equals.addEventListener("clicl", function (3) {
-
-}
+equals.addEventListener("click", function (e) {
+    display.innerHTML = eval(display.innerHTML);
+});
 
