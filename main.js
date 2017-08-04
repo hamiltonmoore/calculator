02501d65fc2fd086350e5b04765d173f37102800
hamilton moore <hamlmoor@gmail.com>
2017-08-04 09:15:44 -0500

    added clear button js

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 var buttons = document.querySelectorAll(".buttons");
 var display = document.querySelector(".display");
-var clear;
-var equals;
+var clear = document.querySelector(".clear");
+var equals = document.querySelector(".equals");
 
 for (i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (e) {
         display.innerHTML += e.target.innerHTML;
 
     });
+}
+
+clear.addEventListener("click", function (e) {
+    display.innerHTML = "";
+
+});
+
+equals.addEventListener("clicl", function (3) {
+
 }
 
