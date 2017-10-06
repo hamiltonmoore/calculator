6df769c9080ef00677ea3db792c1abd142406863
hamilton moore <hamlmoor@gmail.com>
2017-10-06 09:37:35 -0500

    cleaned up file

--- main.js
+++ main.js
@@ -22,7 +22,4 @@ equals.addEventListener("click", function (e) {
     display.innerHTML = eval(display.innerHTML);
     results.push(display.innerHTML);
 });
-// arrow_Up.addEventListener("click", function (e) {
-//     results = display.innerHTML;
-// });
 
